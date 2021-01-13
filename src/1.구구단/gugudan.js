aedd8ca8d7cc7ea3c987a264fa6d6a7cb22acede
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-13 14:56:16 +0900

    react web game: 함수형 setState, ref

--- src/1.구구단/gugudan.js
+++ src/1.구구단/gugudan.js
@@ -12,17 +12,22 @@ class GuGudan extends React.Component {
         }
     }
 
+    /** 함수를 외부로 빼는 이유: 렌더링 시 매번 실행되어 성능이 저하되는 것을 방지하기 위함 */
     // 입력버튼 클릭 시
     onSubmit = (e) => {
         e.preventDefault();
 
         if(parseInt(this.state.value) === parseInt(this.state.first) * parseInt(this.state.second)) {
-            this.setState({
-                result: `${this.state.first} X ${this.state.second} = ${this.state.value} ✔정답!`,
+            this.setState((prevState) => { // 이전 값을 인자로 받아서 사용 가능하다.
+               return {
+                result: `${prevState.first} X ${prevState.second} = ${prevState.value} ✔정답!`,
                 first: Math.ceil(Math.random() * 9),
                 second: Math.ceil(Math.random() * 9),
                 value: ''
+               };
             });
+
+            this.input.focus();
         } else {
             this.setState({
                 result: '땡',
@@ -34,13 +39,18 @@ class GuGudan extends React.Component {
     // 사용자가 값 입력 시
     onChange = (e) => this.setState({value: e.target.value})
 
+    // input focus event: DOM에 직접 접근
+    input; 
+    onRefInput = (c) => { this.input = c; }
+
+    
     render() {
         return (
             //<div></div> 생략 =><></> or <React.Fragment></React.Fragment>
             <> 
                 <div>{this.state.first} 곱하기 {this.state.second}는?</div>
                 <form onSubmit={this.onSubmit}>
-                    <input type="number" value={this.state.value} onChange={this.onChange}/>
+                    <input ref={this.onRefInput} type="number" value={this.state.value} onChange={this.onChange}/>
                     <button>입력!</button>    
                 </form>
                 <div>{this.state.result}</div>
