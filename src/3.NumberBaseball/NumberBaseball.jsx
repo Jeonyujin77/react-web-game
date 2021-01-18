2d18730eee9994818fc27b28f2668b3888841edf
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-18 15:40:59 +0900

    NumberBaseball: 반복문

--- src/3.NumberBaseball/NumberBaseball.jsx
+++ src/3.NumberBaseball/NumberBaseball.jsx
@@ -1,17 +1,43 @@
 import React, {Component} from 'react';
 const { Component } = React;
 
+function getNumbers() {
+
+}
 class NumberBaseball extends Component {
     state = {
+        result: '',
+        value: '',
+        answer: getNumbers(),
+        tries: []
+    };
+
+    onSubmitForm = () => {
+
+    };
+
+    onChangeInput = () => {
 
     };
 
     render() {
         return (
             <>
-                <h1>NumberBaseball</h1>
+                <h1>{this.state.result}</h1>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>
+                    <button>입력!</button>
+                </form>
+                <div>시도: {this.state.tries.length}</div>
+                <ul>
+                    {[].map((v) => {
+                        return (
+                            <li>{v}</li>
+                        );
+                    })}
+                </ul>
             </>
-        )
+        );
     }
 }
 
