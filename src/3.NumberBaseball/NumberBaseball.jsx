17ef492d1e2454e77224752260fe21770c1c9684
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-18 14:52:50 +0900

    NumberBaseball

---
+++ src/3.NumberBaseball/NumberBaseball.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+const { Component } = React;
+
+class NumberBaseball extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <>
+                <h1>NumberBaseball</h1>
+            </>
+        )
+    }
+}
+
+module.exports = NumberBaseball;
