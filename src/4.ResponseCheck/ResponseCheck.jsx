7fbb6636f5d51599c5d061d5d9b8c2bea0f7287c
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-19 17:07:50 +0900

    react web game: setTimeOut넣어 반응속도 체크

--- src/4.ResponseCheck/ResponseCheck.jsx
+++ src/4.ResponseCheck/ResponseCheck.jsx
@@ -8,9 +8,41 @@ class ResponseCheck extends Component {
         result: [] // 평균시간
     };
 
+    timeout; // setTimeOut
+    startTime; // 반응속도-시작
+    endTime; // 반응속도-끝
+
     // 화면 클릭 시 
     onClickScreen = () => {
-
+        const { state, message, result } = this.state;
+        if(state === 'waiting') {
+            this.setState({
+                state: 'ready',
+                message: '초록색이 되면 클릭하세요.'
+            });
+            this.timeout = setTimeout(() => {
+                this.setState({
+                    state: 'now',
+                    message: '지금 클릭!'
+                })
+            }, Math.floor(Math.random() * 1000) + 2000); //2-3초 랜덤
+            this.startTime = new Date();
+        } else if(state === 'ready') { // 성급하게 클릭
+            clearTimeout(this.timeout); // setTimeOut 초기화
+            this.setState({
+                state: 'waiting',
+                message: '이런, 성급하시군요! 초록색이 된 후에 클릭하세요.'
+            });
+        } else if(state === 'now') { // 반응속도 체크
+            this.endTime = new Date();
+            this.setState((prevState) => {
+                return {
+                    state: 'waiting',
+                    message: '클릭해서 시작하세요.',
+                    result: [...prevState.result, this.endTime - this.startTime]
+                }
+            })
+        }
     }
 
     renderAverage = () => {
