da75882e7c2e50873948d35a7b3081512c3c663d
Yujin Jeon <wjsdbwls7777@gmail.com>
2021-01-19 11:13:17 +0900

    react web game: props와 state 연결

--- src/3.NumberBaseball/Try.jsx
+++ src/3.NumberBaseball/Try.jsx
@@ -3,13 +3,26 @@ const { memo } = require('react');
 const React = require('react');
 
 // Class Type
+// PureComponent: state, props 값 변경 시에만 새로 렌더링함 -> 성능 향상
 // class Try extends PureComponent {
 
-//     /*shouldComponentUpdate: state, props 값 변경 시에만 새로 렌더링함 -> 성능 향상
-//         shouldComponentUpdate(nextProps, nextState, nextContext) { 
+//     // shouldComponentUpdate: state, props 값 변경 시에만 새로 렌더링함 -> 성능 향상
+//     //  shouldComponentUpdate(nextProps, nextState, nextContext) { 
+//     // }
+
+//     constructor(props) {
+//         super(props);
+//         // 다른 동작
+//         const filtered = this.props.filter(() => {
+
+//         });
+
+//         this.state = {
+//             result: filtered,
+//             try: this.props.try
+//         }
 //     }
 
-//     PureComponent: state, props 값 변경 시에만 새로 렌더링함 -> 성능 향상*/
 //     render() {
 //         const { tryInfo } = this.props;
 //         return (
