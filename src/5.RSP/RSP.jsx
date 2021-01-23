751405d361606aa24e27db026975759e14f7b0cc
전유진 <wjsdbwls7777@gmail.com>
2021-01-23 15:16:23 +0900

    react web game: RSP

---
+++ src/5.RSP/RSP.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+const { Component } = React;
+
+class RSP extends Component {
+    render() {
+        return(
+            <>
+                <h1>가위바위보</h1>
+            </>
+        )
+    }
+};
+
+module.exports = RSP;
