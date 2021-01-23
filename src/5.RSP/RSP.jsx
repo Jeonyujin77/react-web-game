59c4b250d09763b2ad9e7494b6a19d850ab89c52
전유진 <wjsdbwls7777@gmail.com>
2021-01-23 22:56:51 +0900

    react web game: react life cycle

--- src/5.RSP/RSP.jsx
+++ src/5.RSP/RSP.jsx
@@ -1,11 +1,48 @@
 const React = require('react');
 const { Component } = React;
 
+/*
+* [Life Cycle]
+* - 클래스
+*   constructor -> render -> ref -> componentDidMount
+*   (setState/props 바뀔 때) -> shouldComponentUpdate(true) -> render -> componentDidUpdate
+*   부모가 나를 없앴을 때 -> componentWillUnmount -> Destroy
+*/
 class RSP extends Component {
+    state = {
+        result: '',
+        imgCoord: 0,
+        score: 0
+    };
+
+    /**  */
+    // 컴포넌트가 처음 렌더링 된 후 
+    componentDidMount() {
+
+    }
+
+    // 리렌더링 후
+    componentDidUpdate() {
+
+    }
+
+    // 컴포넌트가 제거되기 전
+    componentWillUnmount() {
+
+    }
+
     render() {
-        return(
+        const { result, score, imgCoord } = this.state;
+        return (
             <>
-                <h1>가위바위보</h1>
+                <div id='computer' style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }}/>
+                <div>
+                    <button id='rock' className='btn' onClick={() => onClickBtn('바위')}>바위</button>
+                    <button id='scissor' className='btn' onClick={() => onClickBtn('가위')}>가위</button>
+                    <button id='paper' className='btn' onClick={() => onClickBtn('보')}>보</button>
+                </div>
+                <div>{result}</div>
+                <div>현재 {score}점</div>
             </>
         )
     }
