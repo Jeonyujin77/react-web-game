da8817416b42e43ff997e1c91337838520caf974
전유진 <wjsdbwls7777@gmail.com>
2021-02-02 00:10:35 +0900

    react web game: useContext를 이용하여 지뢰칸 렌더링

--- src/8.MineSearch/Td.jsx
+++ src/8.MineSearch/Td.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TableContext, CODE } from './MineSearch';
 
-const Td = () => {
-    
+const getTdStyle = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+            return {
+                background: '#444'
+            }
+        case CODE.OPENED:
+            return {
+                background: '#fff'
+            };
+        default:
+            return {
+                background: '#fff'
+            }
+    }
+};
+
+const getTdText = (code) => {
+    switch (code) {
+        case CODE.NORMAL:
+            return '';
+        case CODE.MINE:
+            return 'X';
+        default:
+            return '';
+    }
+};
+
+const Td = ({ rowIndex, cellIndex }) => {
+    const { tableData } = useContext(TableContext);
+
+    return (
+        <td style={getTdStyle(tableData[rowIndex][cellIndex])}>
+            {getTdText(tableData[rowIndex][cellIndex])}
+        </td>
+    );
 };
 
 export default Td;
